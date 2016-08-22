82450d0d1a7b58d7f086097eaaff5cb159c573b2
AlexRedchets <alexey.redchets@gmail.com>
2016-08-22 18:24:26 -0400

    NHT

--- app/src/main/java/com/azvk/nationalhockeyteams/dependencies/NetModule.java
+++ app/src/main/java/com/azvk/nationalhockeyteams/dependencies/NetModule.java
@@ -1,9 +1,31 @@
 package com.azvk.nationalhockeyteams.dependencies;
 
+import android.app.Application;
+
+import javax.inject.Singleton;
+
 import dagger.Module;
+import dagger.Provides;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 @Module
 public class NetModule {
 
-    private String BASE_URL;
+    private String baseUrl;
+
+    public NetModule(String baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+
+    @Provides
+    @Singleton
+    Retrofit provideRetrofit(Application application){
+        return new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .build();
+    }
 }
