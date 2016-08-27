26e7fe6a6861102717ff7b993669174cadcff085
AlexRedchets <alexey.redchets@gmail.com>
2016-08-27 16:20:27 -0400

    NHT

--- app/src/main/java/com/azvk/nationalhockeyteams/presenters/BasePresenter.java
+++ app/src/main/java/com/azvk/nationalhockeyteams/presenters/BasePresenter.java
@@ -1,5 +1,12 @@
 package com.azvk.nationalhockeyteams.presenters;
 
+import android.support.annotation.MainThread;
+
+import rx.Observable;
+import rx.Observer;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 import rx.subscriptions.CompositeSubscription;
 
 public abstract class BasePresenter implements Presenter {
@@ -22,7 +29,7 @@ public abstract class BasePresenter implements Presenter {
     }
 
     private CompositeSubscription configureCompositeSubscription() {
-        if (compositeSubscription == null || compositeSubscription.isUnsubscribed()){
+        if (compositeSubscription == null || compositeSubscription.isUnsubscribed()) {
             compositeSubscription = new CompositeSubscription();
         }
         return compositeSubscription;
@@ -34,9 +41,17 @@ public abstract class BasePresenter implements Presenter {
     }
 
     private void unsubscribeAll() {
-        if (compositeSubscription != null){
+        if (compositeSubscription != null) {
             compositeSubscription.unsubscribe();
             compositeSubscription = null;
         }
     }
+
+    protected <F> void subscribe (Observable<F> observable, Observer<F> observer){
+        Subscription subscription = observable
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe();
+    }
+
 }
